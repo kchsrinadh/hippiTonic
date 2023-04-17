fa923f67490c8e428e170d10d43161cdbed24219
kchsrinadh <96496607+kchsrinadh@users.noreply.github.com>
2023-04-17 14:31:10 -0500

    Init (#6)
    * fix website url.
    
    * point all urls to hippitonic
    
    * lowercase all characters in website urls
    
    * update urls
    
    * fix sub url
    
    * change org name to kchsrinadh

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,17 +13,14 @@ const config = {
   // Set the production url of your site here
 
 
-
   url: 'https://kchsrinadh.github.io',
-
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/hippitonic/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'hippiTonic', // Usually your GitHub org/user name.
-
+  organizationName: 'kchsrinadh', // Usually your GitHub org/user name.
 
 
   projectName: 'hippitonic', // Usually your repo name.
